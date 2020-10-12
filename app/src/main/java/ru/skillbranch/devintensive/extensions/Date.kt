48ce63a93788bcc7b37963c1c8fe964882f1bc15
pokrov <al.n.pokrovsky@gmail.com>
2020-10-12 15:33:25 +0300

    fix plural date

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -18,7 +18,7 @@ enum class TimeUnits(val ms: Int, val russianCases: Array<String>) {
             value % 10 == 1L -> 1
             else -> 0
         }
-        return "${value.absoluteValue/this.ms} ${this.russianCases[case]}"
+        return "${value.absoluteValue} ${this.russianCases[case]}"
     }
 
 }
@@ -44,18 +44,18 @@ fun Date.humanizeDiff(date: Date = Date()) =
         in 0..1.sec -> "только что"
         in 1.sec..45.sec -> "несколько секунд назад"
         in 45.sec..75.sec -> "минуту назад"
-        in 75.sec..45.min -> "${TimeUnits.MINUTE.plural(diff)} назад"
+        in 75.sec..45.min -> "${TimeUnits.MINUTE.plural(diff/TimeUnits.MINUTE.ms)} назад"
         in 45.min..75.min-> "час назад"
-        in 75.min .. 22.hour -> "${TimeUnits.HOUR.plural(diff)} назад"
+        in 75.min .. 22.hour -> "${TimeUnits.HOUR.plural(diff/TimeUnits.HOUR.ms)} назад"
         in 22.hour .. 26.hour -> "день назад"
-        in 27.hour..360L.day -> "${TimeUnits.DAY.plural(diff)} назад"
+        in 27.hour..360L.day -> "${TimeUnits.DAY.plural(diff/TimeUnits.DAY.ms)} назад"
         in (-1).sec..0.sec -> "прямо сейчас"
         in (-45).sec..(-1).sec -> "через несколько секунд"
         in (-75).sec..(-45).sec -> "через минуту"
-        in (-45).min..(-75).sec -> "через ${TimeUnits.MINUTE.plural(diff)}"
+        in (-45).min..(-75).sec -> "через ${TimeUnits.MINUTE.plural(diff/TimeUnits.MINUTE.ms)}"
         in (-75).min..(-45).min -> "через час"
-        in (-22).hour..(-75).min -> "через ${TimeUnits.HOUR.plural(diff)}"
+        in (-22).hour..(-75).min -> "через ${TimeUnits.HOUR.plural(diff/TimeUnits.HOUR.ms)}"
         in (-26).hour..(-22).hour -> "через день"
-        in (-360L).day..(-26).hour -> "через ${TimeUnits.DAY.plural(diff)}"
+        in (-360L).day..(-26).hour -> "через ${TimeUnits.DAY.plural(diff/TimeUnits.DAY.ms)}"
         else -> if (diff > 0) "более года назад" else "более чем через год"
     }
